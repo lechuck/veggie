652ac287343f470eb90b22558126832da4ffb089
Tony Karppinen <tony.karppinen@gmail.com>
2010-11-25 21:46:44 +0200

    Initial commit. Empty project with Authlogic.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Veggie::Application.config.secret_token = '0a666022e46fb64591430712a761f84b7bfc17bba05e95887440d9620596df8a8853750b65deee69ba4648dece2e93975aeb0b32bf38bfaa83891b7f152cab26'
