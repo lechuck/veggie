3fa8d8e199a644e7ebbb6e9f90890fcf40387577
Tony Karppinen <tony.karppinen@gmail.com>
2011-01-24 12:42:02 +0200

    Rudimentary autocomplete for tags

--- app/controllers/tag_autocomplete_searches_controller.rb
+++ app/controllers/tag_autocomplete_searches_controller.rb
@@ -2,10 +2,27 @@ class TagAutocompleteSearchesController < ApplicationController
   
   respond_to :json
   
-  def Index
-    @alltags = %w(abc tag another blah)
-    @alltags = @alltags.to_json
-    respond_with(@alltags)
+  def index
+    #logger.info('no niin' + params[:term]);
+
+    # Search the tags
+    query = "SELECT name FROM tags"
+    unless params[:term].nil?
+      query = query + " WHERE name like '%" + params[:term] + "%'"
+    
+    end
+    
+    #@tags = Restaurant.connection.select_all("SELECT name FROM tags WHERE name like '%aw%'")    
+    #@tags = Restaurant.connection.select_all("SELECT name FROM tags WHERE name like '%params[:term]%'").map{|x| x['name']}
+    @tags = Restaurant.connection.select_all(query).map{|x| x['name']}
+    @tags = @tags.to_json      
+    
+    #@alltags = %w(abc tag another blah)
+    #@alltags = @alltags.to_json
+
+     respond_to do |format|
+       format.json  { render :json => @tags }       
+     end
     
   end
 
